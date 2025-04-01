80a04468746f91e1a95b13286b7bf0560a8bac05
Zyad Wael Khedr <zyadwkhedr@gmail.com>
2025-04-01 22:18:45 +0200

    added go router package for naviagation

---
+++ lib/core/routes.dart
@@ -0,0 +1,21 @@
+import 'package:go_router/go_router.dart';
+import 'package:new_my_pharmacist/features/auth/presentation/login_page.dart';
+
+final GoRouter goRouter = GoRouter(
+  initialLocation: '/',  // This is where your app starts
+  routes: [
+    // GoRoute(
+    //   path: '/',
+    //   builder: (context, state) => HomePage(),
+    // ),
+    GoRoute(
+      path: '/login',
+      builder: (context, state) => LoginPage(),
+    ),
+    // GoRoute(
+    //   path: '/profile',
+    //   builder: (context, state) => ProfilePage(),
+    // ),
+  
+  ],
+);
--- lib/my_app.dart
+++ lib/my_app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:new_my_pharmacist/core/routes.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -7,10 +8,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
-      designSize: Size(375, 812), 
-      minTextAdapt: true, 
+      designSize: Size(375, 812),
+      minTextAdapt: true,
       builder: (context, child) {
-         return const Placeholder();
+        return MaterialApp.router(routerConfig: goRouter);
       },
     );
   }
