89861a316df5883268cdeb6406dfec4118b25a63
Zyad Wael Khedr <zyadwkhedr@gmail.com>
2025-04-01 22:15:58 +0200

    added flutter screenutil

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
